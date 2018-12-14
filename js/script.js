820578d94289481d08d677832370946dedc553fe
RUSsiljoe <russiljoe@mail.ru>
2018-12-14 15:26:01 +0300

    Добавил hover-эффекты

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 menu.onclick = function myFunction() {
   var x = document.getElementById("myTopnav");
+
   if (x.className === "topnav") {
     x.className += " responsive";
-  } 
-  else{
+  } else{
     x.className = "topnav";
   }
 }
